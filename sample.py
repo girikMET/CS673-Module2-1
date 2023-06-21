c5c0d9c84a502c881eb6381deedd11309ed5ffed
girik-met-bu <girik@bu.edu>
2023-06-21 11:11:17 -0400

    Created sample.py
    Initial Commit of File sample.py

---
+++ sample.py
@@ -0,0 +1,7 @@
+class MyClass:
+    def __init__(self):
+        print("This is the Sample Commit file for Module 2 Assignment 1 for METCS 673 Course")
+
+
+if __name__ == "__main__":
+    obj = MyClass()
